0e66a5e9e6d4de6af5ba43c1c3ac8a7d68bc3d1d
草梅友仁 <996881204@qq.com>
2020-06-08 17:06:12 +0800

    修复 require.extension 为 undefined 的 Bug

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const template = require('./lib/index');
 const extension = require('./lib/extension');
 
 template.extension = extension;
-require.extensions[template.defaults.extname] = extension;
+if (require && require.extensions) {
+    require.extensions[template.defaults.extname] = extension;
+}
 
 module.exports = template;
